cc489412cbe989458a9ac1f52f498ac82636cb25
raúl <rsperoni@magnesium.coop>
2021-10-23 20:32:15 -0300

    que cambio

--- src/Polis.js
+++ src/Polis.js
@@ -1,33 +1,35 @@
 import { useEffect, useState } from "react";
-import loadPolis from "./loadPolis";
 
-
-const Polis = ({ uuid, visible }) => {
-
+const Polis = ({}) => {
   const [loaded, setLoaded] = useState(false);
 
   const urlLoader = process.env.REACT_APP_LOADER_URL;
   const conversationId = process.env.REACT_APP_CONVERSATION_ID;
-  //const urlLoader = "https://pol.is/embed.js";
-  //const conversationId = "4dfnw4r9th";
 
   useEffect(() => {
-    loadPolis(urlLoader, () => {
-      setLoaded(true);
-    });
-  }, []);
-
-  
+    const script = document.createElement("script");
+    script.src = urlLoader;
+    script.async = true;
+    document.body.appendChild(script);
+    setLoaded(true);
+    return () => {
+      document.body.removeChild(script);
+      setLoaded(false);
+    };
+  },[]);
 
   return (
     <div>
-      <div
-        id="polisDiv"
-        data-page_id='luc'
-        data-site_id='polis_site_id_Bo4TQ5RdT85PRGv0e1'
-        className={"polis"}
-        data-border={0}
-      ></div>
+      {!loaded && <div>Loading...</div>}
+      {loaded && (
+        <div
+          id="polisDiv"
+          data-page_id="luc"
+          data-site_id="polis_site_id_Bo4TQ5RdT85PRGv0e1"
+          className={"polis"}
+          data-border={0}
+        ></div>
+      )}
     </div>
   );
 };
