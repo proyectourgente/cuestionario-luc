54a9b940b3b5d20cdf506f4af2096a4fef9ffa3d
raúl <rsperoni@magnesium.coop>
2021-11-17 20:19:52 -0300

    cache?

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -31,3 +31,19 @@ self.addEventListener("install", function (event) {
   );
   self.skipWaiting();
 });
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          // Return true if you want to remove this cache,
+          // but remember that caches are shared across
+          // the whole origin
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
