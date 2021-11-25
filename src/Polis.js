beb4cd5fcd30d3ef11afa94cbcdf6f315eb699f4
raúl <rsperoni@magnesium.coop>
2021-11-25 17:55:57 -0300

    cambios

--- src/Polis.js
+++ src/Polis.js
@@ -23,7 +23,7 @@ const Polis = ({ urlLoader, pageId, siteId, site }) => {
           <span className="mr-2">y vos, </span>
           <span className="font-black mt-1">¿QUÉ PENSÁS?</span>
         </h1>
-        <h2 className="pt-3 text-center text-gray-600">Te invitamos a responder estas preguntas y al finalizar vas a poder conocer en qué grupo de opinión estás.</h2>
+        <h2 className="pt-3 text-center text-gray-600">Respondé algunas preguntas sobre la LUC y descubrí en qué grupo de opinión estás.</h2>
       </div>
       {!loaded && <div>Cargando...</div>}
       {loaded && (
@@ -32,7 +32,7 @@ const Polis = ({ urlLoader, pageId, siteId, site }) => {
           data-page_id={pageId}
           data-site_id={siteId}
           data-parent_url={site}
-          className={"polis pb-10 h-full"}
+          className={"polis h-full"}
           data-border={0}
         ></div>
       )}
